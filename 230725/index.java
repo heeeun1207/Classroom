806f9bd07b5c36b2c18b66a3ce269a9a564120ff
jungheeeun <jungheeeun272@gmail.com>
2023-07-25 16:15:59 +0900

    자바!

---
+++ 230725/index.java
@@ -0,0 +1,11 @@
+class Helloworld {
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+  }
+}
+//  javac index.java 실행하면 디렉터리에 Helloworld.class 라는 이름의 파일이 생긴다.
+// 이 파일이 컴파일된 파일이고 , 쉽게 말해서 실행파일이라고 할 수 있다.
+// 컴파일된 파일을 실행시켜주는 프로그램을 이용해야 한다. 이 작업을 하는것을 런처라고 한다.
+
+// heeeun@jeonghuieun-ui-MacBookAir 230725 % java Helloworld 
+// Hello world
---
+++ 230725/org/opentutorials/javatutorials/eclipse/CharString.java
@@ -0,0 +1,10 @@
+package org.opentutorials.javatutorials.eclipse;
+
+public class CharString {
+
+	public static void main(String[] args) {
+		System.out.println("생활코딩이다");
+
+
+	}
+}
