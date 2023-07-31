df017cacec18d33a5a8b310045fdef5eb80b7ef3
jungheeeun <jungheeeun272@gmail.com>
2023-07-31 19:01:01 +0900

    명시적 형 변환

---
+++ 230725/org/opentutorials/javatutorials/variable/ExplicitConversion.java
@@ -0,0 +1,11 @@
+package org.opentutorials.javatutorials.variable;
+
+public class ExplicitConversion {
+	public static void main(String[] args) {
+		float a = (float)100.0;
+		int b = (int)100.0F;
+		System.out.println(a);
+		System.out.println(b);
+		//괄호 안에 데이터 타입을 지정해서 값 앞에 위치시키는 것을 명시적인 형 변환
+	}
+}
