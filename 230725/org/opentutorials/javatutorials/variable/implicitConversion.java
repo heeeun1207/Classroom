d0aeb341fe8fc3f1f910c8a4ccd163270cce9308
jungheeeun <jungheeeun272@gmail.com>
2023-07-31 18:42:17 +0900

    implicit Conversion

---
+++ 230725/org/opentutorials/javatutorials/variable/implicitConversion.java
@@ -0,0 +1,10 @@
+package org.opentutorials.javatutorials.variable;
+
+public class implicitConversion {
+	public static void main(String[] args) {
+	double a = 3.0F;
+	System.out.println(a);
+	//이 때 3.0F의 값은 자동으로 double 타입으로 형 변환이 일어난다. 
+	//타입을 변경해도 정보의 손실이 일어나지 않는 경우 자동 형 변환이 일어난다.
+	}
+}
