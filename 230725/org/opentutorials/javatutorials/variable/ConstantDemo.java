1da41faff88fa482997e32bfaafb4b648ab868b9
jungheeeun <jungheeeun272@gmail.com>
2023-07-31 18:31:27 +0900

    long 명시적 데이터 타입 지정

--- 230725/org/opentutorials/javatutorials/variable/ConstantDemo.java
+++ 230725/org/opentutorials/javatutorials/variable/ConstantDemo.java
@@ -3,7 +3,9 @@ package org.opentutorials.javatutorials.variable;
 public class ConstantDemo {
 	
 		public static void main(String[] args) {
-			double a = 2.2;
+			long a = 2147483648L;
 			System.out.println(a);
+			//long a = 2147483648;
+			// error ->"The literal 2147483648 of type int is out of range"
 		}
 }
