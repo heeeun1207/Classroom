f16d3872cb367c34af3ac17446fba1e3bf23587c
jungheeeun <jungheeeun272@gmail.com>
2023-07-31 18:51:51 +0900

    자동 형 변환이 일어나는 규칙

--- 230725/org/opentutorials/javatutorials/variable/implicitConversion.java
+++ 230725/org/opentutorials/javatutorials/variable/implicitConversion.java
@@ -2,10 +2,14 @@ package org.opentutorials.javatutorials.variable;
 
 public class implicitConversion {
 	public static void main(String[] args) {
-		float a = 3.0; //error
-	System.out.println(a);
-	//이 때 3.0F의 값은 자동으로 double 타입으로 형 변환이 일어난다. 
-	//타입을 변경해도 정보의 손실이 일어나지 않는 경우 자동 형 변환이 일어난다.
-	//자동 형 변환의 원칙은 표현범위가 좁은 데이터 타입에서 넓은 데이터 타입으로의 변환만 허용된다는 것이다.
+		int a = 3;
+		float b = 1.0F;
+		double c = a + b;
+		System.out.println(c);
+		// 1. 형변환 필요
+		// 2. 정수 int → 실수 float
+		// 3. 3. → 3.0F + 1.0F =4.0F
+		// 4. double c 에 맞춰서 형변환 필요
+		// 5. 4.0 (double 데이터타입)
 	}
 }
