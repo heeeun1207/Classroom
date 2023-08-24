797f542ab0ca2980a2d40731e50998eeaa5c41e5
jungheeeun <jungheeeun272@gmail.com>
2023-08-24 20:08:22 +0900

    학생 무작위로 섞어서 팀 나누기

---
+++ 230305/index.js
@@ -0,0 +1,43 @@
+
+/* 
+KDT-2 학생들을 모두 조회해서 팀을 나눠야 함
+랜덤 결정으로 1조 부터 5조( 총원 25명으로 5의 배수 )
+한명이라도 누락되면 안됨
+한명이라도 중복되면 안됨 -> 당장 떠오르는 것 집합개념
+알고리즘 만들기 -> 학생들과 분석하기 -> 코드가 절차적이여야함 
+*/
+
+const studentList = ["앨리스", "밥", "찰리", "데이브", "이브", "프랭크", "그레이스", "행크", "아이비", "잭", "지니", "케이스", "로라", "맥스", "나오미", "올리버", "페넬로페", "퀸", "라이언", "사라", "톰", "우라누스", "비비안", "윌리엄", "엑스"];
+
+// 학생 리스트를 섞기 위한 Fisher-Yates 알고리즘
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+// 학생 리스트를 섞습니다.
+shuffle(studentList);
+
+// 팀 수와 학생 수
+const numTeams = 5;
+const studentsPerTeam = studentList.length / numTeams;
+
+// 팀 별로 나누어진 학생들의 배열
+const teams = [];
+
+// 각 팀에 학생들을 할당합니다.
+for (let i = 0; i < numTeams; i++) {
+  const team = studentList.slice(i * studentsPerTeam, (i + 1) * studentsPerTeam);
+  teams.push(team);
+}
+
+// 결과 출력
+for (let i = 0; i < teams.length; i++) {
+  console.log(`Team ${i + 1}: ${teams[i].join(", ")}`);
+}
+
+// 총 학생 수와 팀 수 확인
+console.log(`Total students: ${studentList.length}`);
+console.log(`Total teams: ${teams.length}`);
--- 230725/org/opentutorials/javatutorials/method/MethodDemo3.java
+++ 230725/org/opentutorials/javatutorials/method/MethodDemo3.java
@@ -2,11 +2,9 @@ package org.opentutorials.javatutorials.method;
  
 public class MethodDemo3 {
     public static void numbering() {
-        int i = 0;
-        while (i < 10) {
-            System.out.println(i);
-            i++;
-        }
+       for(int i = 0; i <10; i ++) {
+    	   System.out.println(i);
+       }
     }
  
     public static void main(String[] args) {
